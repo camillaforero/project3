e1dcf24da1299938b5d5e10e8026ac512f3a0a7e
camillaforero <cforero@hawk.iit.edu>
2024-11-12 21:07:13 -0600

    creating very basic map script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+async function initMap() {
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+
+  const customMapStyle = [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    { featureType: "road", elementType: "geometry", stylers: [{ color: "#38414e" }] },
+    { featureType: "water", elementType: "geometry", stylers: [{ color: "#17263c" }] }
+  ];
+
+  const chicago = { lat: 41.8781, lng: -87.6298 };
+  const map = new Map(document.getElementById("map"), {
+    center: chicago,
+    zoom: 12,
+    styles: customMapStyle,
+  });
 
